1c1568aa60fcc634d7a50fd77ccf6ada64e76d3b
Jin-SukKim <99wlstjr@gmail.com>
2024-11-03 15:20:27 +0900

    Fix: Add random seed

--- code/train.py
+++ code/train.py
@@ -17,6 +17,19 @@ from east_dataset import EASTDataset
 from dataset import SceneTextDataset
 from model import EAST
 
+def set_seed(seed=42):
+    """Sets the seed of the entire notebook so results are the same every time we run.
+    This is for REPRODUCIBILITY."""
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # When running on the CuDNN backend, two further options must be set
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    # Set a fixed value for the hash seed
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    
 def parse_args():
     parser = ArgumentParser()
 
